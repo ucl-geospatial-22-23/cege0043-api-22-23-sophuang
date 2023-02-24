6474adb440f375da2554428768658e874bc819dc
sophuang <yiqi.huang.19@ucl.ac.uk>
2023-02-24 15:24:15 +0000

    Fixed the probelm of now is not defined by using Date.now()

--- dataAPI.js
+++ dataAPI.js
@@ -6,9 +6,25 @@ let path = require("path");
 let app = express();
 let fs = require('fs');
 
-// add an https server to serve files let http = require('http');
+// add an https server to serve files 
+let http = require('http');
 let httpServer = http.createServer(app);
 httpServer.listen(4480);
 app.get('/',function (req,res) {
-res.send("hello world from the Data API "+ now());
+res.send("hello world from the Data API "+ Date.now());
 });
+
+
+// adding functionality to allow cross-origin queries 
+app.use(function(req, res, next) {
+res.setHeader("Access-Control-Allow-Origin", "*"); 
+res.setHeader("Access-Control-Allow-Headers", "X-Requested-With"); 
+res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); 
+next();
+});
+
+// adding functionality to log the requests 
+app.use(function (req, res, next) {
+    let filename = path.basename(req.url); let extension = path.extname(filename);
+    console.log("The file " + filename + " was requested.");
+    next(); });
